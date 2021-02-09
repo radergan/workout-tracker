f6c23c8101659ccae7ad9a3eec4b56274dd63e0c
PJ Dalton <patrickjdalton1@gmail.com>
2021-02-08 20:57:53 -0600

    Different handling on req.body for PUT request

--- routes/routes.js
+++ routes/routes.js
@@ -12,9 +12,9 @@ router.put("/api/workouts/:id", (req, res) => {
     });
 });
 
-router.post("/api/workouts", ({ body }, res) => {
-    console.log(body);
-    Workout.create(body)
+router.post("/api/workouts", (req, res) => {
+    console.log(req.body);
+    Workout.create({day: Date.now(), exercises: req.body})
       .then(dbTransaction => {
         res.json(dbTransaction);
       })
